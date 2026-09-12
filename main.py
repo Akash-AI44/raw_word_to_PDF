42116f443ceb595c1aefe66c5c979e973f602f7d
Anwar Islam Akash <anwar.islam@ajentica.ai>
2026-09-13 00:27:51 +0600

    Update socket server with http response

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ server_socket.bind(("127.0.0.1", 8000))
 
 server_socket.listen()
 
-print("Server is listening...")
+print("Server is running at http://127.0.0.1:8000")
 
 connection, address = server_socket.accept()
 
@@ -14,9 +14,17 @@ print("Client connected:", address)
 
 data = connection.recv(1024)
 
-print("Received:", data)
+print("Request:")
+print(data.decode())
 
-connection.send(b"Hello from server!")
+response = (
+    "HTTP/1.1 200 OK\r\n"
+    "Content-Type: text/html\r\n"
+    "\r\n"
+    "<h1>Hello, I am Akash!</h1>"
+)
+
+connection.send(response.encode())
 
 connection.close()
 server_socket.close()
