8374905cfd493a7df89970fbcbc831931d95b3d5
Anwar Islam Akash <anwar.islam@ajentica.ai>
2026-09-13 14:08:37 +0600

    serve index.html and handle 404

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ class MyHandler(BaseHTTPRequestHandler):
             self.end_headers()
 
             self.wfile.write(html)
+        else:
+            self.send_error(404, message="404 Not Found", explain=None)
 
 
 server = HTTPServer(("127.0.0.1", 8000), MyHandler)
